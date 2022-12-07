f95db8b1b9ec43f51a0abf017e158cc7f2f368f7
Hrabalikova <hrabalikova@gmail.com>
2022-12-07 16:16:42 +0000

    update

--- 3D_draft/js/script.js
+++ 3D_draft/js/script.js
@@ -86,17 +86,6 @@ require([
   });
   
   
-  
-
-
- 
-    
-/**************************************
-* LineOfSight widget
-**************************************/
-//add widget and let......
-
-  
 /***********************************
 * Add the widgets' UI elements to the view
 ***********************************/
@@ -124,8 +113,131 @@ require([
     view: view,
     container: "LayerList"
   });
-//  view.ui.add(layerList, "bottom-right");
+  view.ui.add(layerList, "bottom-right");
 
 
+
+      
+/**************************************
+* LineOfSight widget
+**************************************/
+//add widget and let......
+const lineOfSight = new LineOfSight({
+  view: view,
+  container: "losWidget"
+});
+
+
+
+/**************************************
+ * Add symbols to mark the intersections
+ * for the line of sight
+ **************************************/
+
+  const viewModel = lineOfSight.viewModel;
+
+  // watch when observer location changes
+  viewModel.watch("observer", (value) => {
+    setIntersectionMarkers();
+  });
+
+  // watch when a new target is added or removed
+  viewModel.targets.on("change", (event) => {
+    event.added.forEach((target) => {
+      setIntersectionMarkers();
+      // for each target watch when the intersection changes
+      target.watch("intersectedLocation", setIntersectionMarkers);
+    });
+    event.removed.forEach(() => {
+      // remove intersection markers for removed targets (remove with right click on target)
+      setIntersectionMarkers();
+    });
+  });
+
+  // an inverted cone marks the intersection that occludes the view
+  const intersectionSymbol = {
+    type: "point-3d",
+    symbolLayers: [
+      {
+        type: "object",
+        resource: { primitive: "inverted-cone" },
+        material: { color: [255, 100, 100] },
+        height: 10,
+        depth: 10,
+        width: 10,
+        anchor: "relative",
+        anchorPosition: { x: 0, y: 0, z: -0.7 }
+      }
+    ]
+  };
+
+  function setIntersectionMarkers() {
+    view.graphics.removeAll();
+    viewModel.targets.forEach((target) => {
+      if (target.intersectedLocation) {
+        const graphic = new Graphic({
+          symbol: intersectionSymbol,
+          geometry: target.intersectedLocation
+        });
+        view.graphics.add(graphic);
+      }
+    });
+  }
+
+  /**************************************
+  * Create an analysis by setting
+  * the initial observer and four targets
+  **************************************/
+
+  viewModel.observer = new Point({
+    latitude: 42.3521,
+    longitude: -71.0564,
+    z: 2
+  });
+
+  viewModel.targets = [
+    createTarget(42.3492, -71.0529),
+    createTarget(42.3477, -71.0542),
+    createTarget(42.3485, -71.0533),
+    createTarget(42.3467, -71.0549)
+  ];
+
+  function createTarget(lat, lon, z) {
+    return {
+      location: new Point({
+        latitude: lat,
+        longitude: lon,
+        z: z || 0
+      })
+    };
+  }
+
+  // add an Expand widget to make the menu responsive
+  const expand = new Expand({
+    expandTooltip: "Expand line of sight widget",
+    view: view,
+    content: document.getElementById("menu"),
+    expanded: false
+  });
+
+  view.ui.add(expand, "top-right");
+
+  view.when(() => {
+    // allow user to turn the layer with new planned buildings on/off
+    // and see how the line of sight analysis changes
+    const plannedBuildingsLayer = view.map.layers
+      .filter((layer) => {
+        return (
+          layer.title === "Boston major projects - MajorProjectsBuildings"
+        );
+      })
+      .getItemAt(0);
+
+    document
+      .getElementById("layerVisibility")
+      .addEventListener("change", (event) => {
+        plannedBuildingsLayer.visible = event.target.checked;
+      });
+  });
 
 });
