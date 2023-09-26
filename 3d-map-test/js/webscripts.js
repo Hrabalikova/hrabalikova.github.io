a822fc71c81519b081f879518b2a02ec820dd4e2
hrabalikova <hrabalikova@gmail.com>
2023-09-26 19:02:35 +0000

    drag splash screen

--- 3d-map-test/js/webscripts.js
+++ 3d-map-test/js/webscripts.js
@@ -1,6 +1,9 @@
 function showSplashScreen() {
     var modal = document.getElementById("myModal");
     var span = document.getElementsByClassName("close")[0];
+    var dragArea = document.getElementsByClassName("drag-area")[0];
+    var offsetX, offsetY, isDragging = false;
+
     modal.style.display = "block";
   
     span.onclick = function() {
@@ -14,5 +17,22 @@ function showSplashScreen() {
     }
   }
   
+  // Dragging functionality
+  dragArea.addEventListener('mousedown', function(e) {
+    isDragging = true;
+    offsetX = e.clientX - modal.getBoundingClientRect().left;
+    offsetY = e.clientY - modal.getBoundingClientRect().top;
+  });
 
+  window.addEventListener('mousemove', function(e) {
+    if (isDragging) {
+      modal.style.left = e.clientX - offsetX + 'px';
+      modal.style.top = e.clientY - offsetY + 'px';
+    }
+  });
+
+  window.addEventListener('mouseup', function() {
+    isDragging = false;
+  });
+}
  
